e0ebf67ef0f1a1d8cdda20ff9b489af4952f081b
Alex Bandisch <alexander.bandisch@gmail.com>
2018-04-20 09:02:47 +0100

    Use material ui buttons

--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import { fullWhite } from 'material-ui/styles/colors';
+import RightArrow from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
 import RequiresLogin from '../../RequiresLogin';
 import './Dashboard.css';
+
+const styles = {
+  button: {
+    marginBottom: '20px',
+  },
+};
 
 const Board = ({ onSubmitOwnWorkout, onSubmitTrainerWorkout }) => (
   <section className="dashboard">
@@ -12,19 +21,42 @@ const Board = ({ onSubmitOwnWorkout, onSubmitTrainerWorkout }) => (
     </p>
 
     <form onSubmit={onSubmitOwnWorkout}>
-      <button className="btn btn-start">
-        <i className="fa fa-play" /> My own Workout
-      </button>
+      <RaisedButton
+        label="My own Workout"
+        labelPosition="before"
+        icon={<RightArrow color={fullWhite} />}
+        primary
+        fullWidth
+        type="submit"
+        style={styles.button}
+      />
     </form>
 
     <form onSubmit={onSubmitTrainerWorkout}>
-      <button className="btn btn-start">
-        <i className="fa fa-play" /> Trainer defined Workout
-      </button>
+      <RaisedButton
+        label="Trainer defined Workoutt"
+        labelPosition="before"
+        icon={<RightArrow color={fullWhite} />}
+        primary
+        fullWidth
+        type="submit"
+        style={styles.button}
+      />
     </form>
 
   </section>
 );
+/*
+      <button className="btn btn-start">
+        <i className="fa fa-play" /> My own Workout
+      </button>
+*/
+
+/*
+      <button className="btn btn-start">
+        <i className="fa fa-play" /> Trainer defined Workout
+      </button>
+*/
 
 Board.propTypes = {
   onSubmitOwnWorkout: PropTypes.func.isRequired,
