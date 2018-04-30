2bbf7b05680051589cd9421c8a420a706a103cb2
Alex Bandisch <alexander.bandisch@gmail.com>
2018-04-30 14:19:08 +0100

    LoginForm linting

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -4,8 +4,6 @@ import { compose } from 'recompose';
 import { connect } from 'react-redux';
 import { Field, reduxForm, Form } from 'redux-form';
 import RaisedButton from 'material-ui/RaisedButton';
-import { fullWhite } from 'material-ui/styles/colors';
-import PlayCircleOutline from 'material-ui/svg-icons/av/play-circle-outline';
 import { ScaleLoader } from 'react-spinners';
 import 'font-awesome/css/font-awesome.min.css';
 import { requiredEmail, email, nonEmpty } from '../../utils/formValidators';
